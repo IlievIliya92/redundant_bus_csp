58f8814b09bb33e08282dcaa201759fe947300ba
Iliya Iliev <iliyailiev3592@gmail.com>
2024-11-07 17:26:07 +0200

    scripts: test: Add buss alternating states
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,17 +2,41 @@
 
 set -e
 
+SERVER_BUILD_DIR=./build/server/
+SERVER_BIN=server
+SERVER_CONF_FILE=server_virt.yaml
+
+CLIENT_BUILD_DIR=./build/client/
+CLIENT_BIN=client
+CLIENT_CONF_FILE=client_virt.yaml
+
 run_test() {
-    ./build/server/server -f ./build/server/server_virt.yaml &
-    ./build/client/client -f ./build/client/client_virt.yaml -c 10
+    ${SERVER_BUILD_DIR}${SERVER_BIN} -f ${SERVER_BUILD_DIR}${SERVER_CONF_FILE} &
+    ${CLIENT_BUILD_DIR}${CLIENT_BIN} -f ${CLIENT_BUILD_DIR}${CLIENT_CONF_FILE} -c $1
 }
 
-run_test
+# Both busses up
+run_test $1
 
+# VCAN0 bus down
 sudo ifconfig vcan0 down
-run_test
+run_test $1
 sudo ifconfig vcan0 up
 
+# VCAN1 bus down
 sudo ifconfig vcan1 down
-run_test
+run_test $1
 sudo ifconfig vcan1 up
+
+# Run test
+${SERVER_BUILD_DIR}${SERVER_BIN} -f ${SERVER_BUILD_DIR}${SERVER_CONF_FILE} &
+${CLIENT_BUILD_DIR}${CLIENT_BIN} -f ${CLIENT_BUILD_DIR}${CLIENT_CONF_FILE} -c 100000 &
+
+# Alternate busses state
+sudo ifconfig vcan1 down
+sleep 2
+sudo ifconfig vcan1 up
+sleep 2
+sudo ifconfig vcan0 down
+sleep 2
+sudo ifconfig vcan0 up

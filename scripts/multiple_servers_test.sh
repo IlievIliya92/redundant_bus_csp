0122139f7c27312b4c0e6884284f42a9599fce0f
Iliya Iliev <iliyailiev3592@gmail.com>
2024-11-07 23:37:47 +0200

    scripts: Add multiple servers test
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

---
+++ scripts/multiple_servers_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+SERVER_BUILD_DIR=./build/server/
+SERVER_BIN=server
+
+CLIENT_BUILD_DIR=./build/client/
+CLIENT_BIN=client
+CLIENT_CONF_FILE=client_virt.yaml
+
+${SERVER_BUILD_DIR}${SERVER_BIN} -f ${SERVER_BUILD_DIR}server_virt_addr2.yaml &
+${SERVER_BUILD_DIR}${SERVER_BIN} -f ${SERVER_BUILD_DIR}server_virt_addr3.yaml &
+${SERVER_BUILD_DIR}${SERVER_BIN} -f ${SERVER_BUILD_DIR}server_virt_addr4.yaml &
+${CLIENT_BUILD_DIR}${CLIENT_BIN} -f ${CLIENT_BUILD_DIR}${CLIENT_CONF_FILE} -c 100 -s 1 -a 2 -a 3 -a 4

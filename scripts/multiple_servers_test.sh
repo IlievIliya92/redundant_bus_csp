691ac01f2ad9e906cdf0c3e8a04707ec740514e6
Iliya Iliev <iliyailiev3592@gmail.com>
2024-11-07 23:42:52 +0200

    .github: workflows: ubuntu-latest: Reduce number of test messages
    Signed-off-by: Iliya Iliev <iliyailiev3592@gmail.com>

--- scripts/multiple_servers_test.sh
+++ scripts/multiple_servers_test.sh
@@ -12,4 +12,4 @@ CLIENT_CONF_FILE=client_virt.yaml
 ${SERVER_BUILD_DIR}${SERVER_BIN} -f ${SERVER_BUILD_DIR}server_virt_addr2.yaml &
 ${SERVER_BUILD_DIR}${SERVER_BIN} -f ${SERVER_BUILD_DIR}server_virt_addr3.yaml &
 ${SERVER_BUILD_DIR}${SERVER_BIN} -f ${SERVER_BUILD_DIR}server_virt_addr4.yaml &
-${CLIENT_BUILD_DIR}${CLIENT_BIN} -f ${CLIENT_BUILD_DIR}${CLIENT_CONF_FILE} -c 100 -s 1 -a 2 -a 3 -a 4
+${CLIENT_BUILD_DIR}${CLIENT_BIN} -f ${CLIENT_BUILD_DIR}${CLIENT_CONF_FILE} -c $1 -s 1 -a 2 -a 3 -a 4
